2df33923036ce6b7c2fbefa7fafa11c82dc1b37c
Breno Henrique Nascimento <brenonsc@gmail.com>
2023-11-20 16:41:07 -0300

    Atualização BCrypt

--- 06. BCrypt/main.rb
+++ 06. BCrypt/main.rb
@@ -1,11 +1,4 @@
-require 'bundler/inline'
-
-gemfile true do
-  source 'http://rubygems.org'
-  gem 'bcrypt'
-end
-
-require 'bcrypt'
+require_relative 'crud'
 
 users = [
           { username: "mashrur", password: "password1" },
@@ -15,17 +8,5 @@ users = [
           { username: "heisenberg", password: "password5" }
         ]
 
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
-end
-
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
-
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
-  end
-  list_of_users
-end
+hashed_users = Crud.create_secure_users(users)
+puts hashed_users
